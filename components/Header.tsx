43c75ea67d3ace6d363ce4fe767d231f862fa3c3
HHuzaifa <90773074+HHuzaifa@users.noreply.github.com>
2023-03-29 04:47:54 +0500

    .

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import { BiMenu } from "react-icons/bi";
 import Button from "./Button";
-import { motion } from "framer-motion";
 
 export default function Header() {
   const [showMenu, setShowMenu] = useState(false);
@@ -39,40 +38,55 @@ export default function Header() {
           </Link>
 
           <ul className="hidden lg:flex lg:justify-center space-x-16 font-navLinkFont font-semibold mb-5">
-            <motion.li
-              className="text-white"
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.2 }}
-            >
+            <li className="text-white">
               <a
                 href="#HeroSection"
                 className="hover:text-[#09746F] text-white"
+                onClick={(event) => {
+                  event.preventDefault();
+                  const yOffset = -180; // Set the offset here
+                  const element = document.querySelector("#HeroSection");
+                  if (element) {
+                    const y =
+                      element.getBoundingClientRect().top +
+                      window.pageYOffset +
+                      yOffset;
+                    window.scrollTo({ top: y, behavior: "smooth" });
+                  }
+                }}
               >
                 About
               </a>
-            </motion.li>
-            <motion.li
-              className="text-white"
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.2 }}
-            >
+            </li>
+            <li className="text-white">
               <a
                 href="#ProgramsSection"
                 className="hover:text-[#09746F] text-white"
               >
                 Available Programs
               </a>
-            </motion.li>
+            </li>
 
-            <motion.li
-              className="text-white"
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.2 }}
-            >
-              <a href="#WITSection" className="hover:text-[#09746F] text-white">
+            <li className="text-white">
+              <a
+                href="#WITSection"
+                className="hover:text-[#09746F] text-white"
+                onClick={(event) => {
+                  event.preventDefault();
+                  const yOffset = -100; // Set the offset here
+                  const element = document.querySelector("#WITSection");
+                  if (element) {
+                    const y =
+                      element.getBoundingClientRect().top +
+                      window.pageYOffset +
+                      yOffset;
+                    window.scrollTo({ top: y, behavior: "smooth" });
+                  }
+                }}
+              >
                 WIT
               </a>
-            </motion.li>
+            </li>
           </ul>
           <div className="-mt-6 hidden lg:flex">
             <Link href="https://portal.piaic.org/" target="_blank">
@@ -89,31 +103,55 @@ export default function Header() {
             {showMenu && (
               <div className="absolute top-8 -right-5 -left-24 lg:hidden xl:hidden 2xl:hidden py-16 border-b-2 border-white text-white bg-[#141414] ">
                 <ul className="max-w-full font-navLinkFont font-semibold space-y-10 text-center">
-                  <motion.li className="text-white" whileHover={{ scale: 1.2 }}>
+                  <li className="text-white">
                     <a
                       href="#HeroSection"
                       className="hover:text-[#09746F] text-white"
+                      onClick={(event) => {
+                        event.preventDefault();
+                        const yOffset = -180; // Set the offset here
+                        const element = document.querySelector("#HeroSection");
+                        if (element) {
+                          const y =
+                            element.getBoundingClientRect().top +
+                            window.pageYOffset +
+                            yOffset;
+                          window.scrollTo({ top: y, behavior: "smooth" });
+                        }
+                      }}
                     >
                       About
                     </a>
-                  </motion.li>
-                  <motion.li className="text-white" whileHover={{ scale: 1.2 }}>
+                  </li>
+                  <li className="text-white">
                     <a
                       href="#ProgramsSection"
                       className="hover:text-[#09746F] text-white"
                     >
                       Available Programs
                     </a>
-                  </motion.li>
+                  </li>
 
-                  <motion.li className="text-white" whileHover={{ scale: 1.2 }}>
+                  <li className="text-white">
                     <a
                       href="#WITSection"
                       className="hover:text-[#09746F] text-white"
+                      onClick={(event) => {
+                        event.preventDefault();
+                        const yOffset = -100; // Set the offset here
+                        const element = document.querySelector("#WITSection");
+                        if (element) {
+                          const y =
+                            element.getBoundingClientRect().top +
+                            window.pageYOffset +
+                            yOffset;
+                          window.scrollTo({ top: y, behavior: "smooth" });
+                        }
+                      }}
                     >
                       WIT
                     </a>
-                  </motion.li>
+                  </li>
 
                   <div>
                     <Link href="https://portal.piaic.org/" target="_blank">
